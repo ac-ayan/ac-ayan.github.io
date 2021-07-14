535e823404b738bd313df0b14545809b23890910
Ayan Chakraborty <1905741@kiit.ac.in>
2021-07-15 01:15:28 +0530

    Resized the image assets, added background textures, modified navbar collapse

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -4,5 +4,6 @@ $(function () {
     $(document).scroll(function () {
       var $nav = $(".navbar-section");
       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      $nav.addClass('scrolled-out', $(this).scrollTop() < $nav.height());
     });
   });
