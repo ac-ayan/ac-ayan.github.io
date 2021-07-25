7e50283b8422005244149e9c9dd490b0ccb5bc94
Ayan Chakraborty <1905741@kiit.ac.in>
2021-07-25 13:16:36 +0530

    Changing text-align of project and skill section

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,26 +1,31 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import { Card, Button } from "react-bootstrap";
 import Aos from "aos";
 import "aos/dist/aos.css";
 
 function ProjectCard(props) {
   useEffect(() => {
-    Aos.init({ duration: 1100});
+    Aos.init({ duration: 1100 });
   }, []);
   return (
-    <Card style={{ width: "21rem" }} className="project-card" data-aos="zoom-in">
+    <Card
+      style={{ width: "21rem" }}
+      className="project-card"
+      data-aos="zoom-in"
+    >
       <div className="skill-image">
-      <Card.Img variant="top" src={props.image} className="skill-img"
-      />
+        <Card.Img variant="top" src={props.image} className="skill-img" />
       </div>
       <Card.Body>
         <Card.Title className="project-card-title">{props.title}</Card.Title>
-        <Card.Text>
-          {props.content}
-        </Card.Text>
+        <Card.Text>{props.content}</Card.Text>
         <div className="button-div">
-        <Button variant="primary" href={props.github}>GitHub</Button>
-        <Button variant="primary" href={props.web}>Website</Button>
+          <Button variant="primary" href={props.github}>
+            GitHub
+          </Button>
+          <Button variant="primary" href={props.web}>
+            Website
+          </Button>
         </div>
       </Card.Body>
     </Card>
