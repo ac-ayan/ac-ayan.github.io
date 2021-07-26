e8e5596f9188b3bdb7b35795ceb49bb4c35670bd
Ayan Chakraborty <1905741@kiit.ac.in>
2021-07-26 18:02:07 +0530

    Changed contact button icon and alignment on mobile view

--- src/components/SuccessAlert.js
+++ src/components/SuccessAlert.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import {Button} from 'react-bootstrap';
-import Snackbar from '@material-ui/core/Snackbar';
-import MuiAlert from '@material-ui/lab/Alert';
-import SendIcon from '@material-ui/icons/Send';
-import { makeStyles } from '@material-ui/core/styles';
-
+import React from "react";
+import { Button } from "react-bootstrap";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
+import SendIcon from "@material-ui/icons/Send";
+import { makeStyles } from "@material-ui/core/styles";
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
@@ -12,35 +11,36 @@ function Alert(props) {
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%',
-    '& > * + *': {
+    width: "100%",
+    "& > * + *": {
       marginTop: theme.spacing(2),
     },
   },
 }));
 
-
 export default function SuccessAlert(props) {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
-  
+
   const handleClick = () => {
     setOpen(true);
-    
   };
   const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
 
     setOpen(false);
   };
-  
+
   return (
     <div className={classes.root}>
-      <Button onClick={handleClick} type="submit"> 
-        Send Message <SendIcon />
-      </Button>
+      <div className="mail-send-button">
+        <Button onClick={handleClick} type="submit">
+          Send Message <i class="fas fa-paper-plane"></i>
+          {/* <SendIcon /> */}
+        </Button>
+      </div>
       <Snackbar open={open} autoHideDuration={4000} onClose={handleClose}>
         <Alert onClose={handleClose} severity={props.severity}>
           {props.message}
