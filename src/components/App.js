8364b5482e172b2ccf1c33cb208f1b8a8c8e260b
Ayan Chakraborty <1905741@kiit.ac.in>
2021-07-28 01:16:01 +0530

    Reducing loading effect time

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 3700);
+    setTimeout(() => setLoading(false), 2000);
   }, []);
 
   return (
